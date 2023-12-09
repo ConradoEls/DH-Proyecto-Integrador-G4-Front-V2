e85534ec3b2c76b40f7949e207b36b5456e8d8de
Conrado Els <conradoels@gmail.com>
2023-12-09 18:16:38 -0300

    css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../../assets/logo1.svg';
+import logo from '../../assets/logo1.png';
 import style from './Header.module.css';
 import Menu from './Menu/Menu';
 
@@ -10,9 +10,9 @@ const Header = () => {
       <nav className={style.nav}>
         <div className={style.containerLeft}>
           <Link to="/" aria-label="Página de inicio de Digital Booking">
-            <img src={logo} alt="logo" />
+            <img src={logo} alt="logo" className={style.logoDC}/>
           </Link>
-          <span className={style.motto}>Sentite como en tu hogar</span>
+          <span className={style.motto}>Tu viaje, tu auto, tu libertad</span>
         </div>
         <Menu />
       </nav>
